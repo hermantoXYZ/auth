5f3b8f3b1dbd72416e01b5cc2233020c0155baea
hermantoXYZ <andiupdates@gmail.com>
2025-03-08 20:33:29 +0800

    p

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,8 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-  <div>
-    <h1>Hello</h1>
-    <Button>Hello</Button>
-  </div>
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+ 
+  </main>
   );
 }
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,14 +1,31 @@
-import { betterAuth } from "better-auth";
+import { betterAuth, BetterAuthOptions } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
+import { sendEmail } from "@/actions/email";
+import { openAPI } from "better-auth/plugins";
  
 const prisma = new PrismaClient();
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
         provider: "mysql", // or "mysql", "postgresql", ...etc
     }),
+    plugins: [openAPI()],
     emailAndPassword: {
         enabled: true,
-        autoSignIn: false
-    }
-});
+        autoSignIn: false,
+        requireEmailVerification: true,
+    },
+    emailVerification: {
+        sendOnSignUp: true,
+        autoSignInAfterVerification: true,
+        sendVerificationEmail: async ({ user, token }) => {
+          const verificationUrl = `${process.env.BETTER_AUTH_URL}/api/auth/verify-email?token=${token}&callbackURL=${process.env.EMAIL_VERIFICATION_CALLBACK_URL}`;
+          await sendEmail({
+            to: user.email,
+            subject: "Verify your email address",
+            text: `Click the link to verify your email: ${verificationUrl}`,
+          });
+        },
+      }
+} satisfies BetterAuthOptions);
+
